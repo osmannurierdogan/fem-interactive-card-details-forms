9bd20a9030cf83be63541d70a09834f3a7e0899d
Osman Nuri ERDOGAN <osmerd04@gmail.com>
2022-09-09 04:20:45 +0300

    Input data types have not been checked. AFter confirm will be coded.

---
+++ app.js
@@ -0,0 +1,40 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      // tempCard: {
+      //   number: "0000 0000 0000 0000",
+      //   name: "Jane Appleseed",
+      //   date: {
+      //     month: "12",
+      //     year: "25",
+      //   },
+      //   cvc: "056",
+      // },
+      cardDetails: {
+        number: null,
+        name: null,
+        date: {
+          month: null,
+          year: null,
+        },
+        cvc: null,
+      },
+    };
+  },
+  computed: {
+    splitCardNumber() {
+      let start = 0;
+      let end = 4;
+      let newNumber = "";
+      for (let i = 0; i < 4; i++) {
+        newNumber +=
+          this.cardDetails.number.toString().substring(start, end) + " ";
+        start += 4;
+        end += 4;
+      }
+      return newNumber;
+    },
+  },
+});
+
+app.mount("#app");
